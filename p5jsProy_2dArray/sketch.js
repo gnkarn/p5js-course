031ff1438a857a45e7a801890a882a3aaf23d6ab
gus <gnkarn@gmail.com>
2018-12-05 11:02:05 -0300

    2d arrays

--- p5jsProy_2dArray/sketch.js
+++ p5jsProy_2dArray/sketch.js
@@ -1,7 +1,37 @@
+// ver ejemplo 9.15 
+
+var rows = 10;
+var cols = 10;
+
+var colors = [];
+// crea el array 2d para la matriz
+function make2Darray(cols, rows) {
+	var arr = new Array(cols);
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = new Array(rows);
+	}
+	return arr;
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(300, 300);
+	colors=make2Darray(cols,rows);
+	background(0);
+	fill(200);
+	stroke(255);
+
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			colors[i][j]= random(0, 255);
+		}
+	}
 }
 
 function draw() {
-
+	for (var i = 0; i < cols; i++) {
+		for (var j = 0; j < rows; j++) {
+			fill(colors[i][j]);
+			rect(i * 30, j * 30, 30, 30);
+		}
+	}
 }
