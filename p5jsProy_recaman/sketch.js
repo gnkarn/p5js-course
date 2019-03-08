dddb2f3fc9d1974815666245dcdbc9f136c64f33
gus <gnkarn@gmail.com>
2019-03-08 12:37:28 -0300

    with sound

--- p5jsProy_recaman/sketch.js
+++ p5jsProy_recaman/sketch.js
@@ -1,4 +1,3 @@
-
 //https: //youtu.be/FGC5TdIiT9U
 // en una dimension , al aumentar counter si podemos hacer el salto desde 
 //el estado actual a (actual-counter)>0, entonces va hacia atras 
@@ -6,17 +5,25 @@
 
 let numbers = [false];
 
+var attackLevel = 1.0;
+var releaseLevel = 0;
+
+var attackTime = 0.001;
+var decayTime = 0.5;
+var susPercent = 0.3;
+var releaseTime = 1;
+
 let count = 1;
 sequence = [];
 let index = 0;
 let arcs = [];
 
 class Arc {
-	constructor(start, end, dir,hue) {
+	constructor(start, end, dir, hue) {
 		this.start = start;
 		this.end = end;
 		this.dir = dir;
-		this.hue=hue;
+		this.hue = hue;
 	}
 
 	show() {
@@ -40,7 +47,19 @@ function setup() {
 	background(0);
 	numbers[index] = true;
 	sequence.push(index);
-colorMode(HSB, 255,255,255,255);
+	colorMode(HSB, 255, 255, 255, 255);
+
+
+	env = new p5.Env(); //envelope
+	env.setADSR(attackTime, decayTime, susPercent, releaseTime);
+	env.setRange(attackLevel, releaseLevel);
+
+	osc = new p5.Oscillator();
+	osc.setType('sine');
+	osc.amp(env);
+	osc.start();
+	env.play();
+	frameRate(10);
 }
 
 function step() {
@@ -52,8 +71,10 @@ function step() {
 	sequence.push(next);
 	let a = new Arc(index, next, count % 2, count % 255);
 	arcs.push(a);
-
-
+	let n = index % 25 ;
+	let freq = pow(2, (n ) / 12) * 440; // in hz
+	osc.freq(freq);
+	env.play();
 	index = next;
 	count++;
 
